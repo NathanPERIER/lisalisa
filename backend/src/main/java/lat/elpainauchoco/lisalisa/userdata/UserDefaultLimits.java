514db670f43e3b51acf62b373400c8b95e064f5d
Nathan PERIER <periernathan2000@gmail.com>
2022-05-26 19:54:26 +0200

    Added an object for the default limits of a user

---
+++ backend/src/main/java/lat/elpainauchoco/lisalisa/userdata/UserDefaultLimits.java
@@ -0,0 +1,13 @@
+package lat.elpainauchoco.lisalisa.userdata;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDefaultLimits {
+
+    private CharacterAscensionLimit character;
+    private WeaponAscensionLimit weapon;
+
+}
