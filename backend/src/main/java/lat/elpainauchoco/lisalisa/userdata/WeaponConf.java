f334bb5429c337d12e6b3eb2cf1ca89f5f000ad4
Nathan PERIER <periernathan2000@gmail.com>
2022-05-26 20:14:26 +0200

    Added temporary weapon constructor

--- backend/src/main/java/lat/elpainauchoco/lisalisa/userdata/WeaponConf.java
+++ backend/src/main/java/lat/elpainauchoco/lisalisa/userdata/WeaponConf.java
@@ -13,4 +13,15 @@ public class WeaponConf {
     private int refinement; // 1-5
     private WeaponAscensionLimit limit;
 
+    public WeaponConf() { }
+
+    public WeaponConf(String id, UserConf user) {
+        this.id = id;
+        // Temporary, a generic system would be better
+        level = 1;
+        ascension = 0;
+        refinement = 1;
+        limit = user.getLimit().getWeapon();
+    }
+
 }
