10da3f828e3fa22dd14e1c8b4a228993f955d890
Nathan PERIER <periernathan2000@gmail.com>
2022-05-26 20:16:34 +0200

    Added default limits to user config

--- backend/src/main/java/lat/elpainauchoco/lisalisa/userdata/UserConf.java
+++ backend/src/main/java/lat/elpainauchoco/lisalisa/userdata/UserConf.java
@@ -1,10 +1,14 @@
 package lat.elpainauchoco.lisalisa.userdata;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Getter
+@Setter
 public class UserConf {
 
     private String id;
@@ -17,6 +21,7 @@ public class UserConf {
     @JsonProperty("world_level")
     private int worldLevel;
     private PityConf pity;
+    private UserDefaultLimits limit;
     private Map<String, CharacterConf> characters;
 
     public UserConf() { }
@@ -30,81 +35,10 @@ public class UserConf {
         adventureRank = 1;
         worldLevel = 1;
         pity = new PityConf();
+        limit = new UserDefaultLimits();
         characters = new HashMap<>();
+        characters.put(profile, new CharacterConf(profile, this));
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String n) {
-        name = n;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(final String uid) {
-        id = uid;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(final String d) {
-        desc = d;
-    }
-
-    public String getProfile() {
-        return profile;
-    }
-
-    public void setProfile(final String p) {
-        profile = p;
-    }
-
-    public String getNamecard() {
-        return namecard;
-    }
-
-    public void setNamecard(final String nc) {
-        namecard = nc;
-    }
-
-    public int getAdventureRank() {
-        return adventureRank;
-    }
-
-    public void setNamecard(final int ar) {
-        adventureRank = ar;
-    }
-
-    public int getWorldLevel() {
-        return worldLevel;
-    }
-
-    public void setWorldLevel(final int wl) {
-        worldLevel = wl;
-    }
-
-    public PityConf getPity() {
-        return pity;
-    }
-
-    public void setPity(final PityConf pc) {
-        pity = pc;
-    }
-
-    public Map<String, CharacterConf> getCharacters() {
-        return characters;
-    }
-
-    public void setCharacters(final Map<String, CharacterConf> chars) {
-        characters = chars;
-    }
-
 
     public enum TravelerType {
         BOY,
