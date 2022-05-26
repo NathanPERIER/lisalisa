628837c3be8ccdd08cc0b33f9c6ce37e3fd5b48d
Nathan PERIER <periernathan2000@gmail.com>
2022-05-26 18:29:38 +0200

    Added a controller for the API

---
+++ backend/src/main/java/lat/elpainauchoco/lisalisa/controllers/ApiController.java
@@ -0,0 +1,42 @@
+package lat.elpainauchoco.lisalisa.controllers;
+
+import lat.elpainauchoco.lisalisa.MainApplication;
+import lat.elpainauchoco.lisalisa.dto.CreateUserDTO;
+import lat.elpainauchoco.lisalisa.userdata.UserConf;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.lang.model.type.NullType;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    public ApiController() {
+        MainApplication.verbose();
+    }
+
+    private static final Logger logger = LogManager.getLogger(MainApplication.class);
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<NullType> putUser(@PathVariable(value = "id") final String uid, @RequestBody final CreateUserDTO create) {
+        // TODO
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/conf/{id}")
+    public ResponseEntity<NullType> postConf(@PathVariable(value = "id") final String uid) {
+        // TODO
+        logger.trace(uid);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<UserConf> getTest() {
+        // TODO
+        return ResponseEntity.ok(new UserConf("7000000", "Jean", UserConf.TravelerType.BOY));
+    }
+
+}
