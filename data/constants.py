a84eae4ba1e56dd153822f273f9056cb5f2fdef7
Nathan PERIER <periernathan2000@gmail.com>
2022-06-18 17:54:56 +0200

    Updated character skill and constellation JSONs

--- data/constants.py
+++ data/constants.py
@@ -9,14 +9,15 @@ EXCEL_DIR = os.path.join(REPO_DIR, 'GenshinData/ExcelBinOutput')
 
 
 # Characters
-CHAR_DATA_JSON           = os.path.join(EXCEL_DIR, 'AvatarExcelConfigData.json')
-CHAR_INFO_JSON           = os.path.join(EXCEL_DIR, 'FetterInfoExcelConfigData.json')
-CHAR_SKINS_JSON          = os.path.join(EXCEL_DIR, 'AvatarCostumeExcelConfigData.json')
-CHAR_CURVES_JSON         = os.path.join(EXCEL_DIR, 'AvatarCurveExcelConfigData.json')
-CHAR_ASCENSIONS_JSON     = os.path.join(EXCEL_DIR, 'AvatarPromoteExcelConfigData.json')
-CHAR_SKILLS_JSON         = os.path.join(EXCEL_DIR, 'AvatarSkillExcelConfigData.json')
-CHAR_TALENTS_JSON        = os.path.join(EXCEL_DIR, 'AvatarSkillDepotExcelConfigData.json')
-CHAR_CONSTELLATIONS_JSON = os.path.join(EXCEL_DIR, 'AvatarTalentExcelConfigData.json')
+CHAR_DATA_JSON        = os.path.join(EXCEL_DIR, 'AvatarExcelConfigData.json')
+CHAR_INFO_JSON        = os.path.join(EXCEL_DIR, 'FetterInfoExcelConfigData.json')
+CHAR_SKINS_JSON       = os.path.join(EXCEL_DIR, 'AvatarCostumeExcelConfigData.json')
+CHAR_CURVES_JSON      = os.path.join(EXCEL_DIR, 'AvatarCurveExcelConfigData.json')
+CHAR_ASCENSIONS_JSON  = os.path.join(EXCEL_DIR, 'AvatarPromoteExcelConfigData.json')
+CHAR_SKILLS_JSON      = os.path.join(EXCEL_DIR, 'AvatarSkillExcelConfigData.json')
+CHAR_SKILL_DEPOT_JSON = os.path.join(EXCEL_DIR, 'AvatarSkillDepotExcelConfigData.json')
+CHAR_TALENTS_JSON     = os.path.join(EXCEL_DIR, 'AvatarTalentExcelConfigData.json')
+CHAR_PROUD_SKILL_JSON = os.path.join(EXCEL_DIR, 'ProudSkillExcelConfigData.json')
 
 # Exp
 FRIENDSHIP_JSON = os.path.join(EXCEL_DIR, 'AvatarFettersLevelExcelConfigData.json')
