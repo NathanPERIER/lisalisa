5abb236fd92725faf8b2c8f73d801c4bb462c005
Nathan PERIER <periernathan2000@gmail.com>
2022-06-21 19:21:32 +0200

    Added default translation of item IDs

--- data/items/__init__.py
+++ data/items/__init__.py
@@ -1,8 +1,23 @@
 
-from utils import loadJson
+from utils import loadJson, indexById, idFromName
 from constants import ITEM_LIST_JSON
+from translate.textmap import lang
+from translate.mhy import mhy_items
 
-items = loadJson(ITEM_LIST_JSON)
+import logging
+
+logger = logging.getLogger(__name__)
+
+items = indexById(loadJson(ITEM_LIST_JSON))
 
 
+def __g_supplyThroughItems(hoyo_id: int) -> str :
+	if hoyo_id in items :
+		item = items[hoyo_id]
+		return idFromName(lang[str(item['nameTextMapHash'])])
+	logger.warning("No item found with hoyo id %d", hoyo_id)
+	return str(hoyo_id)
+
+mhy_items.setSupplyStrategy(__g_supplyThroughItems)
+	
 
