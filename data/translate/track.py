de8c3ec111be604675a4f7d9c3be549c599ccd75
Nathan PERIER <periernathan2000@gmail.com>
2022-06-21 19:18:42 +0200

    Added a way to change the supply strategy

--- data/translate/track.py
+++ data/translate/track.py
@@ -8,16 +8,19 @@ class TrackUpdateDict(dict) :
 
 	def __init__(self, supply_startegy = None) :
 		super().__init__()
-		self.supply = supply_startegy if supply_startegy is not None else TrackUpdateDict.__default_strategy
+		self._supply = supply_startegy if supply_startegy is not None else TrackUpdateDict.__default_strategy
 	
 	def __default_strategy(key) :
 		logger.warning("Translation not found for entry %s", str(key))
 		return key
 	
+	def setSupplyStrategy(self, supply_strategy) :
+		self._supply = supply_strategy
+	
 	def __getitem__(self, key) :
 		str_key = str(key)
 		if str_key not in self :
-			value = self.supply(str_key)
+			value = self._supply(str_key)
 			super().__setitem__(str_key, value)
 			return value
 		return super().__getitem__(str_key)
