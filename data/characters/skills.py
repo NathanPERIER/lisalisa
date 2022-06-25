5d4a044da418292b3e285435875f938c599636b8
Nathan PERIER <periernathan2000@gmail.com>
2022-06-25 15:01:36 +0200

    Added comments for the character skills, which is the trickiest part yet

--- data/characters/skills.py
+++ data/characters/skills.py
@@ -19,11 +19,13 @@ proud_skills = loadJson(CHAR_PROUD_SKILL_JSON)
 
 
 def readSkillsConstellations(char: Character) :
+    # Search for the entry corresponding to the character in the skill depot
     depot_search = [x for x in skill_depot if x['id'] == char.skill_depot_id]
     if len(depot_search) != 1 :
         logger.error('No skills found for %s (%d)', char.name, char.hoyo_id)
         return
     depot = depot_search[0]
+    # Talents 
     skills = {
         'normal_attack': __g_readSkill(depot['skills'][0]),
         'elemental_skill': __g_readSkill(depot['skills'][1]),
@@ -31,36 +33,57 @@ def readSkillsConstellations(char: Character) :
         'alternate_sprint': None
     }
     if depot['skills'][2] != 0 :
-        skills['alternate_sprint'] = __g_readSkill(depot['skills'][2])
+        skills['alternate_sprint'] = __g_readSkill(depot['skills'][2], True)
     if depot['skills'][3] != 0 :
         logger.info('Fourth skill slot (id: %d) used by character %s (%d)', depot['skills'][3], char['name'], char['hoyo_id'])
     char.talents = skills
-    passives = [
+    # Passives
+    char.passives = [
         __g_readSimpleSkill(talent['proudSkillGroupId'], talent['needAvatarPromoteLevel'] if 'needAvatarPromoteLevel' in talent else None)
         for talent in depot['inherentProudSkillOpens']
         if 'proudSkillGroupId' in talent
     ]
-    char.passives = passives
+    # Constellations
     char.constellations = __g_readConstellations(depot['talents'])
 
 
+# Read the constellations of a character from the object that stores all the constellations
 def __g_readConstellations(const_ids: "list[int]") -> list :
     res = []
     constellations = {c['talentId']: c for c in talents if c['talentId'] in const_ids}
     if len(constellations) != 6 :
         logger.warning('Expected 6 constellations, found %d', len(constellations))
-    for const_id in const_ids :
-        const = constellations[const_id]
+    for cst_id in const_ids :
+        cst = constellations[cst_id]
         res.append({
-            'name_hash': const['nameTextMapHash'],
-            'name': lang[str(const['nameTextMapHash'])],
-            'desc_hash': const['descTextMapHash'],
-            'desc': lang[str(const['descTextMapHash'])]
+            'name_hash': cst['nameTextMapHash'],
+            'name': lang[str(cst['nameTextMapHash'])],
+            'desc_hash': cst['descTextMapHash'],
+            'desc': lang[str(cst['descTextMapHash'])]
         })
     return res
 
 
-def __g_readSkill(skill_id: int) -> dict :
+# Read a passive from the object that stores all the skills
+# Note : skill is the generic term for passive + talent
+def __g_readSimpleSkill(psg_id: int, ascension: int = None) :
+    skill_search = [psk for psk in proud_skills if psk['proudSkillGroupId'] == psg_id]
+    if len(skill_search) != 1 :
+        logger.error('No skill found with group id %d', psg_id)
+        return None
+    skill = skill_search[0]
+    res = {
+        'name_hash': skill['nameTextMapHash'],
+        'desc_hash': skill['descTextMapHash'],
+        'ascension': ascension
+    }
+    res['name'] = lang[str(res['name_hash'])]
+    res['desc'] = lang[str(res['desc_hash'])]
+    return res
+
+
+# Read a talent from the object that stores all the skills
+def __g_readSkill(skill_id: int, is_sprint = False) -> dict :
     skill_search = [x for x in skills if x['id'] == skill_id]
     if len(skill_search) != 1 :
         logger.error('No skill found with id %d', skill_id)
@@ -76,31 +99,19 @@ def __g_readSkill(skill_id: int) -> dict :
     }
     res['name'] = lang[str(res['name_hash'])]
     res['desc'] = lang[str(res['desc_hash'])]
-    res['costs'], res['stats'] = __g_readProudSkillGroup(skill['proudSkillGroupId'])
-    return res
-
-
-def __g_readSimpleSkill(psg_id: int, ascension: int = None) :
-    skill_search = [psk for psk in proud_skills if psk['proudSkillGroupId'] == psg_id]
-    if len(skill_search) != 1 :
-        logger.error('No skill found with group id %d', psg_id)
-        return None
-    skill = skill_search[0]
-    res = {
-        'name_hash': skill['nameTextMapHash'],
-        'desc_hash': skill['descTextMapHash'],
-        'ascension': ascension
-    }
-    res['name'] = lang[str(res['name_hash'])]
-    res['desc'] = lang[str(res['desc_hash'])]
+    # A talent is associated with a "proud skill group" that contains one entry
+    # per level this talent can have, with the stats of this talent
+    expected_num = 1 if is_sprint else 15
+    res['costs'], res['stats'] = __g_readProudSkillGroup(skill['proudSkillGroupId'], expected_num)
     return res
     
 
-def __g_readProudSkillGroup(psg_id: int) -> "tuple[list,dict]" :
+# Reads the entries of a proud skill group to get the stats + ascension costs of a talent at each level
+def __g_readProudSkillGroup(psg_id: int, expected_num: int) -> "tuple[list,dict]" :
     group = [psk for psk in proud_skills if psk['proudSkillGroupId'] == psg_id]
     group.sort(key = lambda psk: psk['level'])
-    if len(group) != 15 :
-        logger.warning('Expected 15 proud skills in group %d, found %d', psg_id, len(group))
+    if len(group) != expected_num :
+        logger.warning('Expected %d proud skills in group %d, found %d', expected_num, psg_id, len(group))
     costs = []
     stats = {
         'names': None,
@@ -115,11 +126,8 @@ def __g_readProudSkillGroup(psg_id: int) -> "tuple[list,dict]" :
     return costs[1:10], stats
         
 
-# nameTextMapHash ?
-# descTextMapHash ?
-# unlockDescTextMapHash ?
-# breakLevel must be the level required to unlock, not very useful here
-# proudSkillType => 3 for burst, ...
+# Reads an individual entry (i.e. level) in the proud skill group
+# This entry will be called a "proud skill"
 def __g_readProudSkill(psk: dict) -> "tuple[dict,int,dict]" :
     cost = {
         c['id']: c['count']
@@ -129,7 +137,14 @@ def __g_readProudSkill(psk: dict) -> "tuple[dict,int,dict]" :
     mora_cost = psk['coinCost'] if 'coinCost' in psk else 0
     stats = __g_readStats(psk['paramDescList'], psk['paramList'])
     return cost, mora_cost, stats
+# nameTextMapHash ?
+# descTextMapHash ?
+# unlockDescTextMapHash ?
+# breakLevel must be the level required to unlock, not very useful here
+# proudSkillType => 3 for burst, ...
 
+
+# Reads the stats for a proud skill
 def __g_readStats(desc: "list[int]", values: "list[float]") -> dict :
     res = {}
     for d in desc :
@@ -138,6 +153,12 @@ def __g_readStats(desc: "list[int]", values: "list[float]") -> dict :
             __g_readParam(dsc, values, res)
     return res
 
+
+# Reads an individual stat in a proud skill
+# A stat has a description with the format {name}|{value}
+# The value has a special format that requires to read numbers
+# at certain indexes in a list
+# These numbers also need to be formatted, which is done in a separate method
 def __g_readParam(desc: str, values: "list[float]", params: dict) :
     desc_split = desc.split('|')
     if len(desc_split) != 2 :
@@ -150,16 +171,27 @@ def __g_readParam(desc: str, values: "list[float]", params: dict) :
         desc_repl = params_reg.sub(val, desc_repl, 1)
     params[name] = desc_repl
 
+
+# Formats a number for the value of a stat entry
+# The values have a type described by a string
+# The exact specifications of the type are unclear
+# but this code should be about right
 def __g_formatValue(val: float, type: str) -> str :
+    # I is for an integer (no decimals)
     if type == 'I' :
         return str(int(val))
+    # a P at the end is for a percentage, so we multiply the value by 100
     if type.endswith('P') :
         val = val * 100
+    # at this point, we consider only floating point values w/ 2 digit precision
     res = str(round(val, 2))
+    # we remove the useless tailing zeroes
     while res.endswith('0') :
         res = res[:-1]
     if res.endswith('.') :
         res = res[:-1]
+    # if we have a percentage, we add the percent sign at the end
+    # (else it is most likely a float, so we don't need to do naything else)
     if type.endswith('P') :
         res = f"{res}%"
     return res
