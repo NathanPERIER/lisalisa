7a55151626330b12645905b3e244dd8a982e4efe
Nathan PERIER <periernathan2000@gmail.com>
2022-06-26 12:58:31 +0200

    Fixed bad identifier field

--- data/collection/common/cities.py
+++ data/collection/common/cities.py
@@ -5,7 +5,7 @@ from translate.textmap import lang
 from translate.mhy import mhy_cities
 
 def __g_filterCities(cities: list) -> list :
-	return [x for x in cities if x['cityId'] < 100]
+	return [x for x in cities if x['cityId'] < 90]
 
 cities = __g_filterCities(loadJson(WORLD_CITIES_JSON))
 
@@ -14,10 +14,10 @@ def readCities() -> "dict[str,dict]" :
 	res = {}
 	for data in cities :
 		city = {
-			'hoyo_id': data['id'],
+			'hoyo_id': data['cityId'],
 			'name_hash': data['cityNameTextMapHash']
 		}
-		city['name'] = lang[str(data['name_hash'])]
+		city['name'] = lang[str(city['name_hash'])]
 		identifier = idFromName(city['name'])
 		mhy_cities[city['hoyo_id']] = identifier
 		res[identifier] = city
