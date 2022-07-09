1f1f8c3ba5e4fe34a83d2ce577901c1268cc669f
Nathan PERIER <periernathan2000@gmail.com>
2022-07-09 16:57:31 +0200

    Added artifact parsing (close #4)

--- data/collection/artifacts/__init__.py
+++ data/collection/artifacts/__init__.py
@@ -1,119 +1,75 @@
 
 from utils import fieldsEqual, idFromName, loadJson, indexById, groupByField
-from constants import ARTIFACT_DATA_JSON, ARTIFACT_SETS_JSON, ARTIFACT_EQUIP_JSON
+from constants import ARTIFACT_CODEX_JSON, ARTIFACT_DISPLAY_JSON
 from translate.textmap import lang
-from artifacts.curves import curves
+from translate.mhy import mhy_art_sets
+from artifacts.pieces import readArtifactPiece as __g_readArtifactPiece
+from artifacts.sets   import readArtifactSet   as __g_readArtifactSet
+from artifacts.curves import curves, getSetAffixes as __g_getSetAffixes
 
 import logging
-from enum import Enum
 
 logger = logging.getLogger(__name__)
 
-class EquipType(Enum) :
-    EQUIP_BRACER   = 'flower_of_life'
-    EQUIP_NECKLACE = 'plume_of_death'
-    EQUIP_SHOES    = 'sands_of_eon'
-    EQUIP_RING     = 'goblet_of_eonothem'
-    EQUIP_DRESS    = 'circlet_of_logos'
 
-def __g_filterSets(sets: "list[dict[str,any]]") -> "list[dict[str,any]]" :
-    return [
-        art_set for art_set in sets
-        if 'DisableFilter' not in art_set or art_set['DisableFilter'] != 1
-    ]
+def __g_formatDisplay(display: "list[dict[str,any]]") -> "dict[int,list[dict[int,dict[str,any]]]]" :
+    res = {}
+    filtered = [x for x in display if 'param' in x]
+    for param, values in groupByField(filtered, 'param').items() :
+        res[param] = indexById(values, 'rankLevel')
+    return res
 
-artifacts = loadJson(ARTIFACT_DATA_JSON)
-sets = __g_filterSets(loadJson(ARTIFACT_SETS_JSON))
-equips = groupByField(loadJson(ARTIFACT_EQUIP_JSON), 'id')
+codex = loadJson(ARTIFACT_CODEX_JSON)
+display = __g_formatDisplay(loadJson(ARTIFACT_DISPLAY_JSON))
+
+PIECE_FIELDS = ["cupId", "leatherId", "capId", "flowerId", "sandId"]
 
 
-def getCurves() -> list :
+def getArtifactCurves() -> list :
     return curves
 
 
 def readArtifactSets() :
-    res = []
-    pieces = __g_readAllPieces(artifacts)
-    for art_set in sets :
-        data = __g_readArtifactSet(art_set)
-        set_pieces = pieces[data['hoyo_id']]
-        data['pieces'] = __g_groupArtifactPieces(set_pieces)
-        res.append(data)
-    return res
-
-
-def __g_groupArtifactPieces(pieces: "list[dict[str,any]]") :
     res = {}
-    for type, t_pieces in groupByField(pieces, 'type').items() :
-        grouped = groupByField(t_pieces, 'rarity')
-        grouped_items = list(grouped.items())
-        grouped_items.sort(key = lambda x: x[0])
-        t_res = [x[1][0] for x in grouped_items]
-        for rarity, g_pieces in grouped.items() :
-            if not fieldsEqual(g_pieces, 'name') :
-                logger.warning("Pieces of set %d, of rarity %d and of type %s have different names", g_pieces[0]['set_id'], rarity, type.value)
-                logger.warning([x['name'] for x in g_pieces])
-        res[type.value] = t_res
+    grouped = groupByField(codex, 'suitId')
+    for set_id, data in grouped.items() :
+        art_set = __g_readArtifactSet(set_id)
+        art_set['pieces'] = []
+        for rarity_set in data :
+            __g_readRaritySet(art_set, rarity_set)
+        art_set['pieces'].sort(key = lambda x: x['rarity'])
+        if not fieldsEqual(art_set['pieces'], 'name') :
+            logger.warning("Artifact set %d has different names at different rarities", art_set['hoyo_id'])
+        art_set['name'] = art_set['pieces'][0]['name']
+        identifier = idFromName(art_set['name'])
+        res[identifier] = art_set
+        mhy_art_sets[art_set['hoyo_id']] = identifier
     return res
         
- 
 
-def __g_readAllPieces(artifacts: "list[dict]") -> "dict[str,list[dict[str,any]]]" :
-    res = [
-        __g_readArtifactPiece(piece)
-        for piece in artifacts
-    ]
-    return groupByField(res, 'set_id')
+def __g_readRaritySet(art_set, r_set) :
+    rarity = r_set['level']
+    pieces = []
+    for field in PIECE_FIELDS :
+        if field in r_set :
+            piece_id = r_set[field]
+            pieces.append(__g_readArtifactPiece(piece_id))
+    # Set name
+    disp = display[art_set['hoyo_id']][rarity]
+    display_id = disp['id']
+    name_hash = disp['nameTextMapHash']
+    name = lang(name_hash)
+    # Affixes
+    if not fieldsEqual(pieces, 'prop_id') :
+        logger.warning("Pieces of set %d at rarity %d have different prop IDs", art_set['hoyo_id'], rarity)
+    prop_id = pieces[0]['prop_id']
+    affixes = __g_getSetAffixes(prop_id)
+    art_set['pieces'].append({
+        'display_id': display_id,
+        'name_hash': name_hash,
+        'name': name,
+        'rarity': rarity,
+        'pieces': pieces,
+        'substats': affixes
+    })
 
-
-# TODO : set name ?
-def __g_readArtifactSet(art_set: dict) -> "dict[str,any]" :
-    res = {
-        'hoyo_id': art_set['setId'],
-        'equip_id': art_set['EquipAffixId'],
-        'piece_ids': art_set['containsList'] # Not actually pieces, most likely a group of them
-    }
-    need_nums = art_set['setNeedNum']
-    equip_data = equips[res['equip_id']]
-    if len(need_nums) != len(equip_data) :
-        logger.warning("Found %d equips for artifact set %d, expected %d", 
-                            len(equip_data), res['hoyo_id'], len(need_nums))
-    equip_data.sort(key = lambda x: x['level'] if 'level' in x else 0)
-    res['bonuses'] = [
-        __g_readEquip(equip, need_num)
-        for need_num, equip in zip(need_nums, equip_data)
-    ]
-    return res
-
-
-def __g_readEquip(equip: dict, need_num: int) -> "dict[str,any]" :
-    res = {
-        'hoyo_id': equip['affixId'],
-        'name_hash': equip['nameTextMapHash'],
-        'desc_hash': equip['descTextMapHash'],
-        'nb_req': need_num
-    }
-    res['name'] = lang(res['name_hash'])
-    res['desc'] = lang(res['desc_hash'])
-    return res
-
-
-def __g_readArtifactPiece(piece: dict) -> "dict[str,any]" :
-    res = {
-        'hoyo_id': piece['id'],
-        'story_id': piece['storyId'] if 'storyId' in piece else None,
-        'gadget_id': piece['gadgetId'],
-        'set_id': piece['setId'] if 'setId' in piece else None,
-        'name_hash': piece['nameTextMapHash'],
-        'desc_hash': piece['descTextMapHash'],
-        'type': EquipType[piece['equipType']],
-        'rarity': piece['rankLevel'],
-        'max_level': piece['maxLevel'] - 1,
-         # Levels at which a prop is added (substat level)
-        'add_prop_levels': [x-1 for x in piece['addPropLevels']],
-         # Exp given by using this artifact (at min level) to enhance another artifact
-        'conv_exp': piece['baseConvExp']
-    }
-    res['name'] = lang(res['name_hash'])
-    res['desc'] = lang(res['desc_hash'])
-    return res
