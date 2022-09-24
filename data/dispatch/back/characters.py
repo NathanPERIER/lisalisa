74eb5125553944f8a6a09591df57c10252642542
Nathan PERIER <periernathan2000@gmail.com>
2022-09-24 17:03:13 +0200

    Added constellations that increase burst or skill for characters

--- data/dispatch/back/characters.py
+++ data/dispatch/back/characters.py
@@ -3,6 +3,10 @@ from constants import CHARACTERS_FOLDER, CHARACTERS_IGNORED
 from engine.core import DispatchEngine, Dispatcher
 from engine.dispatchers.merge import MergeDispatcher
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 BACK_CHARACTERS_FILE = 'characters.json'
 
 
@@ -17,10 +21,30 @@ def dispatchCharacters(engine: DispatchEngine) :
 
 
 def __d_transformCharacter(_dispatcher: Dispatcher, character: "dict[str,any]") :
+	elemental_skill = character['talents']['elemental_skill']['name']
+	elemental_burst = character['talents']['elemental_burst']['name']
+	skill_increase_text = f"Increases the Level of {elemental_skill} by 3."
+	burst_increase_text = f"Increases the Level of {elemental_burst} by 3."
+	c3: str = character['constellations'][2]['desc']
+	c5: str = character['constellations'][4]['desc']
+	increase_skill = 10
+	increase_burst = 10
+	if c3.startswith(skill_increase_text) :
+		increase_skill = 3
+		increase_burst = 5
+		if not c5.startswith(burst_increase_text) :
+			logger.warning("Found skill increase at C3 but no burst increase at C5 for %s", character['name'])
+	elif c5.startswith(skill_increase_text) :
+		increase_skill = 5
+		increase_burst = 3
+		if not c3.startswith(burst_increase_text) :
+			logger.warning("Found skill increase at C5 but no burst increase at C3 for %s", character['name'])
 	return {
 		'weapon': character['weapon'],
-		'default_weapon': character['default_weapon'],
-		'alt_skins': __d_transformSkins(character['skins'])
+		'defaultWeapon': character['default_weapon'],
+		'alternateSkins': __d_transformSkins(character['skins']),
+		'increaseSkill': increase_skill,
+		'increaseBurst': increase_burst
 	}
 
 def __d_transformSkins(skins: "dict[str,any]") -> "dict[str,any]" :
